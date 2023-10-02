9b361f0d352d57486fd565c2c6f98f079be2d59b
SB1807 <student1.pj@gmail.com>
2023-10-02 19:43:56 +0300

    Initial commit

---
+++ src/Lab2/ex1.java
@@ -0,0 +1,16 @@
+package Lab2;
+
+
+
+public class ex1 {
+    public static void main(String[] args) {
+        //1
+        //tablou de stringuri
+        //sortare cu sort()
+        //2
+        //cautare pozitie judet dupa nume
+        //metoda de cautare binara din clasa array
+
+
+    }
+}
