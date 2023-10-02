b0d8e13ef58401a1155c6c89d8dad833bf36e84e
SB1807 <student1.pj@gmail.com>
2023-10-02 20:07:32 +0300

    ex4 creare clasa si functie

--- src/Lab2/ex4.java
+++ src/Lab2/ex4.java
@@ -1,5 +1,33 @@
 package Lab2;
+class Persoana{
+    private String nume;
+    private String cnp;
+    public int verificare_cnp(String incercare){
+        String numar_standard_control="279146358279";
+        int suma=0;
+        if (incercare.length()!=13)
+            return 0;
+        for (int i=0;i<13;i++)
+            if("0123456789".indexOf(incercare.charAt(i)) == -1)
+                return 0;
+        if("1256".indexOf(incercare.charAt(0)) == -1)
+            return 0;
+        for(int i=0;i<13;i++)
+        {
+            int a=Character.getNumericValue((incercare.charAt(i)));
+            int b=Character.getNumericValue((numar_standard_control.charAt(i)));
+            suma=suma+(a*b)
+        }
+return 1;
+    }
+    public Persoana(String _nume, String _cnp){
+        nume=_nume;
+        //if (verificare_cnp(_cnp)==1)
+        //{
 
+        //}
+    }
+}
 public class ex4 {
     public static void main(String[] args) {
 
